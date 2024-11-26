a1816d4212fbff00cd87fa0d04eb273083075251
qingli.hql <qingli.hql@alibaba-inc.com>
2024-11-26 21:08:34 +0800

    delete redundant

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,66 +1,30 @@
-import { Suspense, useCallback, useEffect, useRef, useState } from "react";
-import { Box, Portal, useToast } from "@chakra-ui/react";
-import {
-  loadDBs,
-  mediaTable,
-} from "./db-tables/WorkspaceDB";
-import { Topbar } from "./topbar/Topbar";
-import React from "react";
-const WorkflowChat = React.lazy(() => import("./workflowChat/workflowChat"));
+import { Box, Portal, Button } from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
 import { DRAWER_Z_INDEX } from "./const";
-import { api, app } from "./utils/comfyapp";
+const WorkflowChat = React.lazy(() => import("./workflowChat/workflowChat"));
 
 export default function App() {
-  const [loadingDB, setLoadingDB] = useState(true);
   const workspaceContainerRef = useRef(null);
   const [showChat, setShowChat] = useState(false);
 
-
-  const graphAppSetup = async () => {
-    // localStorage.removeItem("workspace");
-    // localStorage.removeItem("comfyspace");
-    try {
-      await loadDBs();
-    } catch (error) {
-      console.error("🦄Workspace Manager - Error loading db", error);
-    }
-    setLoadingDB(false);
-  };
-
-
-  useEffect(() => {
-    /**
-     * because we have turned on strict mode, useEffect will be executed twice in strict mode in the development environment.
-     * and graphAppSetup contains DB related operations, repeated execution will bring some bad results.
-     * so in development environment mode, the first execution is skipped.
-     */
-    graphAppSetup();
-  }, []);
-
-  if (loadingDB) {
-    return null;
-  }
-
   return (
-      <div ref={workspaceContainerRef} className="workspace_manager">
-        <Portal containerRef={workspaceContainerRef}>
-          <Box
-            style={{
-              position: "absolute",
-              top: 0,
-              right: 0,
-              left: 0,
-            }}
-            zIndex={DRAWER_Z_INDEX}
-            draggable={false}
-          >
-            <Topbar 
-              onChatClick={() => setShowChat(true)}
-              showChat={showChat}
-            />
-            {showChat && <WorkflowChat onClose={() => setShowChat(false)} />}
-          </Box>
-        </Portal>
-      </div>
+    <div ref={workspaceContainerRef} className="workspace_manager">
+
+      <Portal containerRef={workspaceContainerRef}>
+        <Box
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            left: 0,
+          }}
+          zIndex={DRAWER_Z_INDEX}
+          draggable={false}
+        >
+          <Button onClick={() => setShowChat(true)}>show chat</Button>
+          {showChat && <WorkflowChat onClose={() => setShowChat(false)} />}
+        </Box>
+      </Portal>
+    </div>
   );
 }
